0ca53ecea74e2a691a12795f704a7939b03313c9
candyflossuk <ross.a.humphrey@googlemail.com>
2021-07-22 10:26:27 +0100

    Added examples of smoke tests and the ability to set up test componenets using pytest.fixture(autouse)

--- ch2/tasks_proj/tests/func/test_api_exceptions.py
+++ ch2/tasks_proj/tests/func/test_api_exceptions.py
@@ -1,6 +1,7 @@
 import pytest
 import tasks
 
+
 def test_add_raises():
     """add() should raise an exception with wrong type param"""
     with pytest.raises(TypeError):
@@ -12,3 +13,16 @@ def test_start_tasks_db_raises():
         tasks.start_tasks_db('some/great/path', 'mysql')
     exception_msg = excinfo.value.args[0]
     assert exception_msg == "db_type must be a 'tiny' or 'mongo'"
+
+@pytest.mark.smoke
+def test_list_raises():
+    """list() should raise an exception with wrong type param"""
+    with pytest.raises(TypeError):
+        tasks.list_tasks(owner=123)
+
+@pytest.mark.get
+@pytest.mark.smoke
+def test_get_raises():
+    """get() should raise an exception with the wrong type param"""
+    with pytest.raises(TypeError):
+        tasks.get(task_id=123)
