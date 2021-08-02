18b28c7d65fd260d6b7222e218dd838a80c200db
candyflossuk <ross.a.humphrey@googlemail.com>
2021-08-02 17:31:13 +0100

    Added more tests expected to fail

--- ch2/tasks_proj/tests/func/test_unique_id_1.py
+++ ch2/tasks_proj/tests/func/test_unique_id_1.py
@@ -30,3 +30,19 @@ def test_unique_id_1():
     id_2 = tasks.unique_id()
     assert id_1 != id_2
 
+
+@pytest.mark.xfail()
+def test_unique_id_is_a_duck():
+    """Demonstrate xfail."""
+    uid = tasks.unique_id()
+    assert uid == 'a duck'
+
+
+@pytest.mark.xfail()
+def test_unique_id_is_not_a_duck():
+    """Demonstrate xpass"""
+    uid = tasks.unique_id()
+    assert uid != 'a duck'
+
+
+
