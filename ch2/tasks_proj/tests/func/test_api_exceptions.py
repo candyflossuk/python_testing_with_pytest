ed233297555efb416b06eb1e7212838543639aa6
candyflossuk <ross.a.humphrey@googlemail.com>
2021-08-05 11:20:33 +0100

    Added example of parameterized testing

--- ch2/tasks_proj/tests/func/test_api_exceptions.py
+++ ch2/tasks_proj/tests/func/test_api_exceptions.py
@@ -26,3 +26,16 @@ def test_get_raises():
     """get() should raise an exception with the wrong type param"""
     with pytest.raises(TypeError):
         tasks.get(task_id=123)
+
+class TestUpdate():
+    """Tests expected exceptions with tasks.update()"""
+
+    def test_bad_id(self):
+        """A non-int id should raise an exception"""
+        with pytest.raises(TypeError):
+            tasks.update(task_id={'dict instead': 1}, task=tasks.Task())
+
+    def test_bad_task(self):
+        """A non-Task task should raise an exception"""
+        with pytest.raises(TypeError):
+            tasks.update(task_id=1, task='not a task')
