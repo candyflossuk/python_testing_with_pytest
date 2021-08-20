ecabf5bc4063df524c62b98d0580081c11e74963
candyflossuk <ross.a.humphrey@googlemail.com>
2021-08-20 17:28:02 +0100

    Added example of a fixture and using this within the tests

--- ch3/test_fixtures.py
+++ ch3/test_fixtures.py
@@ -25,3 +25,13 @@ pytest will look for fixture names within the module OR conftest.py
 
 - Do not import conftest.py from anywhere.
 """
+
+@pytest.fixture()
+def a_tuple():
+    """Return something more interesting"""
+    return 1, 'foo', None, {'bar':23}
+
+
+def test_a_tuple(a_tuple):
+    """Demo the a_tuple fixture."""
+    assert a_tuple[3]['bar'] == 32
