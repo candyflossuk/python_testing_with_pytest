044941bb59dbc97bcb3c842a1a9988bee5abfbc0
candyflossuk <ross.a.humphrey@googlemail.com>
2021-08-23 09:28:31 +0100

    Added an example of using a fixture that uses mutliple fixutres

--- ch3/a/tasks_proj/tests/func/test_add.py
+++ ch3/a/tasks_proj/tests/func/test_add.py
@@ -2,6 +2,7 @@ import pytest
 import tasks
 from tasks import Task
 
+
 def test_add_returns_valid_id(tasks_db):
     """tasks.add(<valid task>) should return an integer"""
     # GIVEN an initialized tasks db
@@ -10,3 +11,13 @@ def test_add_returns_valid_id(tasks_db):
     new_task = Task('do something')
     task_id = tasks.add(new_task)
     assert isinstance(task_id, int)
+
+
+def test_add_increases_count(db_with_3_tasks):
+    """Test tasks.add() affect on tasks.count()."""
+    # GIVEN a db with 3 tasks
+    # WHEN another task is added
+    tasks.add(Task('throw a party'))
+
+    # THEN the count increases by 1
+    assert tasks.count() == 4
